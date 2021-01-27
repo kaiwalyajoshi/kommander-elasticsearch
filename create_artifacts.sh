d61fe9a4d02050a501a05d1dcbeab32186db4923
Kaiwalya Joshi <kjoshi@d2iq.com>
2021-01-26 23:24:11 -0800

    Add prometheus-elasticsearch-exporter 4.1.0 and bump chart to 0.3.0

--- create_artifacts.sh
+++ create_artifacts.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+# Add requisite repositories
+helm repo add elastic https://helm.elastic.co
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
 # Fetch upstream Elastic
-helm pull upstream-elastic/elasticsearch --version 7.10.0
+helm pull elastic/elasticsearch --version 7.10.0
+helm pull prometheus-community/prometheus-elasticsearch-exporter --version 4.1.0
 
 # Move depedencies in
 mv elasticsearch-7.10.0.tgz charts/
+mv prometheus-elasticsearch-exporter-4.1.0.tgz charts/
 
 # Lint directory
 helm lint .
